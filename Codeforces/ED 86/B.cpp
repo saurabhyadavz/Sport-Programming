18065f13b36f6efc9b223282d689131cceb436ec
saurabhshadow <saurabhshadow30@gmail.com>
2020-05-02 19:47:45 +0530

    Add CF

---
+++ Codeforces/ED 86/B.cpp
@@ -0,0 +1,110 @@
+/*
+𝓐𝓾𝓽𝓱𝓸𝓻:-
+𝓢𝓪𝓾𝓻𝓪𝓫𝓱𝓼𝓱𝓪𝓭𝓸𝔀
+------------------------
+*/
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define loop(i,a,b) for(int i=a;i<b;i++)
+#define loopb(i,a,b) for(int i=a;i>=b;i--)
+#define fore(name)  for(auto it=name.begin();it!=name.end();it++)
+#define w()  int t;cin>>t;while(t--)
+#define pb(x) push_back(x)
+#define in(y) insert(y)
+#define MAX 100000
+#define bitscount 32
+#define make(x,y) make_pair(x,y)
+#define LOAD_FACTOR_set(name) name.reserve(32768);name.max_load_factor(0.25);
+#define LOAD_FACTOR_map(name) name.reserve(1024);name.max_load_factor(0.25);
+#define lcm(a,b) ((a*b))/(__gcd(a,b))
+#define int long long int
+#define REDBULL ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define mod 1000000007
+#define endl "\n"
+
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+#define ordered_justset tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+
+struct HASH {
+    size_t operator()(const pair<int, int>&x)const {
+        return ((int)x.first) ^ (((int)x.second) << 32);
+    }
+};
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+
+
+int visited[200001];
+vector<int> v1[200001];
+int dist[3][200001];
+
+void bfs(int u, int d, int id) {
+
+    queue<int> q;
+    visited[u] = 1;
+    q.push(u);
+    dist[id][u] = d;
+    while (q.size() != 0) {
+        int p = q.front();
+        q.pop();
+        for (int i : v1[p]) {
+            if (visited[i] != 1) {
+                dist[id][i] = dist[id][p] + 1;
+                visited[i] = 1;
+                q.push(i);
+            }
+        }
+    }
+
+}
+
+int32_t main()
+{
+
+    REDBULL
+    w() {
+
+        string s;
+        cin >> s;
+        int countb = 0, counta = 0;
+        loop(i, 0, s.size()) {
+            if (s[i] == '1')
+                countb++;
+            else
+                counta++;
+        }
+        if (counta == s.size() || countb == s.size()) {
+            cout << s << s << endl;
+        }
+        else {
+            loop(i, 0, 2 * s.size()) {
+                if (i % 2 != 0) {
+                    cout << 1;
+                }
+                else
+                    cout << 0;
+            }
+            cout << endl;
+        }
+
+
+    }
+
+    return 0;
+}
