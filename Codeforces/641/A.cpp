a0a663d7f69350593c67b823cff396f131d7ed33
saurabhshadow <saurabhshadow30@gmail.com>
2020-05-12 22:46:54 +0530

    CF 641

---
+++ Codeforces/641/A.cpp
@@ -0,0 +1,99 @@
+
+/*
+𝓐𝓾𝓽𝓱𝓸𝓻:-
+𝓢𝓪𝓾𝓻𝓪𝓫𝓱𝓼𝓱𝓪𝓭𝓸𝔀
+------------------------
+*/
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define loop(i,a,b) for(int i=a;i<b;i++)
+#define loopb(i,a,b) for(int i=a;i>=b;i--)
+#define fore(name)  for(auto it=name.begin();it!=name.end();it++)
+#define w()  int t;cin>>t;while(t--)
+#define pb(x) push_back(x)
+#define in(y) insert(y)
+#define MAX 100000
+#define bitscount 32
+#define make(x,y) make_pair(x,y)
+#define LOAD_FACTOR_set(name) name.reserve(32768);name.max_load_factor(0.25);
+#define LOAD_FACTOR_map(name) name.reserve(1024);name.max_load_factor(0.25);
+#define lcm(a,b) ((a*b))/(__gcd(a,b))
+#define int long long int
+#define REDBULL ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define mod 1000000007
+#define endl "\n"
+
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+#define ordered_justset tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+
+struct HASH {
+    size_t operator()(const pair<int, int>&x)const {
+        return ((int)x.first) ^ (((int)x.second) << 32);
+    }
+};
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+vector<int>  printDivisors(int n)
+{
+    vector<int> v;
+    // Note that this loop runs till square root
+    for (int i = 1; i <= sqrt(n); i++)
+    {
+        if (n % i == 0)
+        {
+            // If divisors are equal, print only one
+            if (n / i == i)
+                v.pb(i);
+
+            else
+                v.pb(i), v.pb(n / i); // Otherwise print both
+            // printf("%d %d ", i, n / i);
+        }
+    }
+    sort(v.begin(), v.end());
+    return v;
+}
+
+
+
+int32_t main()
+{
+
+    REDBULL
+    w()
+    {
+        int n, k;
+        cin >> n >> k;
+        int ans;
+        if (n % 2 == 0)
+        {
+            ans = n + (k ) * 2;
+        }
+        else {
+            vector<int > v = printDivisors(n);
+            int first = v[1];
+            k--;
+            ans = first + n + (k ) * 2;
+        }
+        cout << ans << endl;
+
+    }
+
+    return 0;
+}
