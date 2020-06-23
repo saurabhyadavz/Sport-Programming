682474e08413fbbdcfb93df4d4a270e94807b7ae
saurabhshadow <saurabhshadow30@gmail.com>
2020-06-24 00:05:22 +0530

    CF-651

---
+++ Codeforces/652/3.cpp
@@ -0,0 +1,94 @@
+/*
+𝓐𝓾𝓽𝓱𝓸𝓻:-
+𝓢𝓪𝓾𝓻𝓪𝓫𝓱𝓼𝓱𝓪𝓭𝓸𝔀
+------------------------
+*/
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define loop(i,a,b) for(int i=a;i<b;i++)
+#define loopb(i,a,b) for(int i=a;i>=b;i--)
+#define fore(name)  for(auto it=name.begin();it!=name.end();it++)
+#define w  int t;cin>>t;while(t--)
+#define pb(x) push_back(x)
+#define in(y) insert(y)
+#define bitscount 32
+#define make(x,y) make_pair(x,y)
+#define LOAD_FACTOR_set(name) name.reserve(32768);name.max_load_factor(0.25);
+#define LOAD_FACTOR_map(name) name.reserve(1024);name.max_load_factor(0.25);
+#define lcm(a,b) ((a*b))/(__gcd(a,b))
+#define int long long int
+#define REDBULL ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define mod 1000000007
+#define endl "\n"
+#define all(x) (x).begin(), (x).end()
+
+
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+#define ordered_justset tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+
+struct HASH {
+  size_t operator()(const pair<int, int>&x)const {
+    return ((int)x.first) ^ (((int)x.second) << 32);
+  }
+};
+struct custom_hash {
+  static uint64_t splitmix64(uint64_t x) {
+    x += 0x9e3779b97f4a7c15;
+    x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+    x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+    return x ^ (x >> 31);
+  }
+
+  size_t operator()(uint64_t x) const {
+    static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+    return splitmix64(x + FIXED_RANDOM);
+  }
+};
+typedef pair<int, int> pi;
+#define UNMAP(name) unordered_map<int,int,custom_hash> name
+#define MAX 10000000000000
+
+void solve()
+{
+  int n, k;
+  cin >> n >> k;
+  int a[n];
+  loop(i, 0, n)
+  cin >> a[i];
+  sort(a, a + n, greater<int>());
+  int b[k];
+  int cnt = 0;
+  loop(i, 0, k)
+  {
+    cin >> b[i];
+    if (b[i] == 1)
+      cnt++;
+  }
+
+  sort(b, b + k, greater<int>());
+  int ans = 0;
+
+  int l = cnt;
+  int r = n - 1;
+  for (int i = 0; i < k - cnt; i++)
+  {
+    ans += (a[l] + a[r]);
+    l++;
+    r -= (b[i] - 1);
+  }
+  loop(i, 0, cnt)
+  ans += 2 * a[i];
+  cout << ans << endl;
+}
+int32_t main()
+{
+  REDBULL
+  w
+  solve();
+
+  return 0;
+}
