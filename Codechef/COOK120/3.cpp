29bcf7d4c9e4a76bd35df3e8818099c95997656e
saurabhshadow <saurabhshadow30@gmail.com>
2020-07-20 12:43:57 +0530

    COOK120

--- Codechef/COOK120/3.cpp
+++ Codechef/COOK120/3.cpp
@@ -54,16 +54,7 @@ typedef pair<int, int> pi;
 
 void solve()
 {
-	int n;
-	cin >> n;
-	for (int i = 0; i < n - 1; i++)
-	{
-		int u, v;
-		cin >> u >> v;
-	}
-	for (int i = 0; i < n; i++)
-		cout << 1 << " ";
-	cout << endl;
+
 }
 int32_t main()
 {
